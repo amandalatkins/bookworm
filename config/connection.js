683c3d48e11735d5b8a78a1ee20441d26218f543
Amanda Atkins <amanda@digitalrainstorm.com>
2020-01-20 18:57:41 -0800

    Fix errors in connections script

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 if (process.env._ && process.env._.indexOf("heroku")) {
     var connectionDetails = {
         host: "us-cdbr-iron-east-05.cleardb.net",
-        port: ,
+        port: 3306,
         user: "b99175fad98522",
         password: "571db4b3",
         database: "heroku_2cbe3492304daf9"     
@@ -21,13 +21,7 @@ if (process.env._ && process.env._.indexOf("heroku")) {
 
 
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: "bookworm"
-});
+const db = mysql.createConnection(connectionDetails);
 
 db.connect(err => {
     if (err) throw err;
