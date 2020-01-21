125cfdd45863d4bd42318ef3d062503e3fee50ee
Amanda Atkins <amanda@digitalrainstorm.com>
2020-01-21 14:20:04 -0800

    Update heroku db credentials

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 
-if (process.env._ && process.env._.indexOf("heroku")) {
+// if (process.env._ && process.env._.indexOf("heroku")) {
     var connectionDetails = {
         host: "rtzsaka6vivj2zp1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         port: 3306,
@@ -8,16 +8,16 @@ if (process.env._ && process.env._.indexOf("heroku")) {
         password: "kkarftwz4hfl7oro",
         database: "on3qd1e5yxfkuj2p"     
     }
-} else {
-    require("dotenv").config();
-    var connectionDetails = {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: "bookworm"
-    }
-}
+// } else {
+//     require("dotenv").config();
+//     var connectionDetails = {
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//         user: process.env.DB_USER,
+//         password: process.env.DB_PASS,
+//         database: "bookworm"
+//     }
+// }
 
 const db = mysql.createConnection(connectionDetails);
 
