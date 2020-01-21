345cd58ce5e8aaa42e0e95b2a92c9dd7b540fefa
Amanda Atkins <amanda@digitalrainstorm.com>
2020-01-20 18:53:14 -0800

    Add credentials for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,25 @@
 const mysql = require("mysql");
-require("dotenv").config();
+
+if (process.env._ && process.env._.indexOf("heroku")) {
+    var connectionDetails = {
+        host: "us-cdbr-iron-east-05.cleardb.net",
+        port: ,
+        user: "b99175fad98522",
+        password: "571db4b3",
+        database: "heroku_2cbe3492304daf9"     
+    }
+} else {
+    require("dotenv").config();
+    var connectionDetails = {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: "bookworm"
+    }
+}
+
+
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
